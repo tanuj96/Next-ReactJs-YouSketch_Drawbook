1b64af99480f63528b496c293689c1d9a83ed88a
tanuj96 <tanujmittal96@gmail.com>
2023-12-22 13:52:21 +0530

    canvas draaw implementations by CANVAS APIS

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 
 const Board = () => {
     const canvasRef = useRef(null);
+    const shouldDraw = useRef(false);
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
     const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
 
@@ -11,10 +12,59 @@ const Board = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
+        const changeConfig = (color, size) => {
+            context.strokeStyle = color
+            context.lineWidth = size
+        }
+
+        changeConfig(color, size);
+    }, [color, size]);
+
+
+    //Before browser pain
+    useLayoutEffect(() => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
 
         // when mounting
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+
+        const beginPath = (x, y) => {
+            context.beginPath()
+            context.moveTo(x, y)
+        }
+
+        const drawLine = (x, y) => {
+            context.lineTo(x, y);
+            context.stroke();
+        }
+        const handleMouseDown = (e) => {
+            shouldDraw.current = true;
+            beginPath(e.clientX, e.clientY)
+        }
+
+        const handleMouseMove = (e) => {
+            if (!shouldDraw.current) return
+            drawLine(e.clientX, e.clientY);
+        }
+
+        const handleMouseUp = (e) => {
+            shouldDraw.current = false
+        }
+
+
+        canvas.addEventListener('mousedown', handleMouseDown)
+        canvas.addEventListener('mousemove', handleMouseMove)
+        canvas.addEventListener('mouseup', handleMouseUp)
+
+        return () => {
+            canvas.removeEventListener('mousedown', handleMouseDown)
+            canvas.removeEventListener('mousemove', handleMouseMove)
+            canvas.removeEventListener('mouseup', handleMouseUp)
+        }
     }, [])
 
     return (
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -9,7 +9,7 @@ const Toolbox = () => {
 
     const dispatch = useDispatch();
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
-    const { color } = useSelector((state) => state.toolbox[activeMenuItem])
+    const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
     const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
     const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER;
 
@@ -38,7 +38,7 @@ const Toolbox = () => {
             {showBrushToolOption && (<div className={styles.toolItem}>
                 <h4 className={styles.toolText} > Brush Size</h4>
                 <div className={styles.itemContainer}>
-                    <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} />
+                    <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size} />
                 </div>
             </div >)}
         </div >
