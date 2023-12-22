c341774a32537978a1d1f8e0912d25efcee9b52b
tanuj96 <tanujmittal96@gmail.com>
2023-12-22 13:08:42 +0530

    toolbox state , reducer , slice and active class

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,10 @@
 import { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 
 const Board = () => {
     const canvasRef = useRef(null);
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+    const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
 
     useEffect(() => {
         if (!canvasRef.current) return
@@ -19,4 +22,4 @@ const Board = () => {
     )
 }
 
-export default Board;
+export default Board;                                                   
