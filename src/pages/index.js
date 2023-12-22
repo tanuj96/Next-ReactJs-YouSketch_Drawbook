860ac031d3033c771869429308218c0a56ad6b53
tanuj96 <tanujmittal96@gmail.com>
2023-12-22 12:28:40 +0530

    added communication between Menu and Toolbox with Redux toolkit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Board from "@/components/Board";
 import Menu from "@/components/Menu";
 import Toolbox from "@/components/Toolbox";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Menu />
       <Toolbox />
+      <Board />
     </>
   )
 }
